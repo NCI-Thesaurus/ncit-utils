a72367d255756ccf180d7bcd089de2fe028d309b
Jim Balhoff <jim@balhoff.org>
2017-07-26 13:49:44 -0400

    Try to improve performance with custom axiom generation.

--- src/main/scala/org/renci/ncit/MaterializePropertyExpressions.scala
+++ src/main/scala/org/renci/ncit/MaterializePropertyExpressions.scala
@@ -3,6 +3,7 @@ package org.renci.ncit
 import java.io.File
 import java.util.UUID
 
+import scala.annotation.tailrec
 import scala.collection.JavaConverters._
 
 import org.backuity.clist._
@@ -15,12 +16,11 @@ import org.semanticweb.owlapi.model.IRI
 import org.semanticweb.owlapi.model.OWLAxiom
 import org.semanticweb.owlapi.model.OWLClass
 import org.semanticweb.owlapi.model.OWLObjectProperty
-import org.semanticweb.owlapi.model.parameters.Imports
-import org.semanticweb.owlapi.util.InferredAxiomGenerator
-import org.semanticweb.owlapi.util.InferredEquivalentClassAxiomGenerator
-import org.semanticweb.owlapi.util.InferredOntologyGenerator
-import org.semanticweb.owlapi.util.InferredSubClassAxiomGenerator
 import org.semanticweb.owlapi.model.OWLOntology
+import org.semanticweb.owlapi.model.parameters.Imports
+import org.semanticweb.owlapi.reasoner.Node
+import org.semanticweb.owlapi.reasoner.OWLReasoner
+
 import com.typesafe.scalalogging.LazyLogging
 
 object MaterializePropertyExpressions extends Command(description = "Materialize property expressions") with LazyLogging {
@@ -40,9 +40,12 @@ object MaterializePropertyExpressions extends Command(description = "Materialize
     val classes = ontology.getClassesInSignature(Imports.INCLUDED).asScala.toSet
     val axioms = properties.flatMap { property =>
       logger.info(s"Processing property: $property")
-      inferAxioms(classes.flatMap(createAxioms(property, _)), ontology)
+      val propertyAxioms = classes.flatMap(createAxioms(property, _))
+      val inferredAxioms = inferAxioms(propertyAxioms, ontology)
+      propertyAxioms ++ inferredAxioms
     }
-    val expressionsOntology = manager.createOntology(axioms.asJava, IRI.create(s"$prefix/expressions"))
+    val newAxioms = axioms.filterNot(ontology.containsAxiomIgnoreAnnotations(_))
+    val expressionsOntology = manager.createOntology(newAxioms.asJava, IRI.create(s"$prefix/expressions"))
     manager.saveOntology(expressionsOntology, new RioTurtleDocumentFormat(), IRI.create(outputFile))
   }
 
@@ -60,12 +63,29 @@ object MaterializePropertyExpressions extends Command(description = "Materialize
     val expressionsOntology = manager.createOntology(startAxioms.asJava)
     manager.applyChange(new AddImport(expressionsOntology, factory.getOWLImportsDeclaration(ontology.getOntologyID.getOntologyIRI.get)))
     val reasoner = new ElkReasonerFactory().createReasoner(expressionsOntology)
-    val generator = new InferredOntologyGenerator(reasoner, List[InferredAxiomGenerator[_ <: OWLAxiom]](new InferredSubClassAxiomGenerator(), new InferredEquivalentClassAxiomGenerator()).asJava)
-    generator.fillOntology(factory, expressionsOntology)
+    //    val generator = new InferredOntologyGenerator(reasoner, List[InferredAxiomGenerator[_ <: OWLAxiom]](new InferredSubClassAxiomGenerator(), new InferredEquivalentClassAxiomGenerator()).asJava)
+    val newAxioms = traverse(List(reasoner.getTopClassNode), reasoner, Set.empty)
+    //generator.fillOntology(factory, expressionsOntology)
     reasoner.dispose()
-    val newAxioms = expressionsOntology.getAxioms(Imports.EXCLUDED).asScala.toSet
+    //val newAxioms = expressionsOntology.getAxioms(Imports.EXCLUDED).asScala.toSet
     manager.removeOntology(expressionsOntology)
     newAxioms
+  }
+
+  @tailrec
+  def traverse(nodes: List[Node[OWLClass]], reasoner: OWLReasoner, acc: Set[OWLAxiom]): Set[OWLAxiom] = nodes match {
+    case Nil                               => acc
+    case node :: rest if node.isBottomNode => traverse(rest, reasoner, acc)
+    case node :: rest =>
+      val subclassNodes = reasoner.getSubClasses(node.getRepresentativeElement, true)
+      val superclasses = node.getEntities.asScala
+      val subclasses = subclassNodes.getFlattened.asScala
+      val axioms = for {
+        superclass <- superclasses
+        subclass <- subclasses
+        if (!subclass.isOWLNothing)
+      } yield (subclass SubClassOf superclass)
+      traverse(subclassNodes.getNodes.asScala.toList ::: rest, reasoner, acc ++ axioms)
   }
 
 }
